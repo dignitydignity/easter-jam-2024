7fae115176c6ddfc179bee6a73e51fdf83c0a35e
angelo <angelo.marney@gmail.com>
2024-03-14 23:45:42 -0500

    Add player scene

--- main/main.gd
+++ main/main.gd
@@ -2,7 +2,9 @@ extends Node3D
 class_name Main
 
 static var cam : Camera3D
+static var player : Player
 static var rng := RandomNumberGenerator.new()
 
 func _ready():
-	cam = %Camera
+	player = %Player
+	cam = %Player/Camera
