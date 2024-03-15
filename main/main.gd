f2242623ef92dc4f577723cc14c8d22105052e0f
angelo <angelo.marney@gmail.com>
2024-03-14 23:14:12 -0500

    Fix rabbit random color code.

--- main/main.gd
+++ main/main.gd
@@ -1,13 +1,8 @@
 extends Node3D
 class_name Main
 
-@onready var cam : Camera3D = %Camera
+static var cam : Camera3D
+static var rng := RandomNumberGenerator.new()
 
-func _process(_delta):
-	# TODO: This might lead to performance issues.
-	# Instead, maybe we should inject the "cam" dependency only
-	# when a new rabbit is spawned.
-	var rabbits = find_children("","Rabbit")
-	for r in rabbits:
-		var r_as_rabbit = r as Rabbit
-		r_as_rabbit.cam = cam
+func _ready():
+	cam = %Camera
--- rabbit/rabbit.gd
+++ rabbit/rabbit.gd
@@ -1,19 +1,24 @@
 extends Node3D
 class_name Rabbit
 
-var cam : Camera3D
-
 @onready var _rabbit_body : MeshInstance3D = %rabbit_body
 @onready var _rabbit_tail : MeshInstance3D = %rabbit_tail
 
 func _ready() -> void:
-	var mat_body := _rabbit_body.get_surface_override_material(0)
-	var mat_tail := _rabbit_tail.get_surface_override_material(0)
-	var rand_color := Color(randf(), randf(), randf())
-	mat_body.set("albedo_color", rand_color)
-	mat_tail.set("albedo_color", rand_color)
+	# Randomize the rabbit's color.
+	var mat_body := _rabbit_body.get_surface_override_material(0) as ORMMaterial3D
+	var mat_tail := _rabbit_tail.get_surface_override_material(0) as ORMMaterial3D
+	var rand_color := Color(Main.rng.randf(), Main.rng.randf(), Main.rng.randf())
+	# Random color must be bright for face details to be visible.
+	while rand_color.get_luminance() < 0.9:
+		rand_color = Color(Main.rng.randf(), Main.rng.randf(), Main.rng.randf())
+	mat_body.albedo_color = rand_color
+	mat_tail.albedo_color = rand_color
+	# Materials shouldn't be shared so that rabbit colors are unique.
+	assert(mat_body.is_local_to_scene())
+	assert(mat_tail.is_local_to_scene())
 
 func _process(_delta) -> void:
-	# Tail is 2D and should billboard.
-	_rabbit_tail.look_at(cam.global_position)
+	# "Billboard" the 2D tail sprite.
+	_rabbit_tail.look_at(Main.cam.global_position)
 	
