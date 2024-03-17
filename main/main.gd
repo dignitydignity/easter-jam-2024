897b182d1118171cc417b420269d0ef271ded7f9
angelo <angelo.marney@gmail.com>
2024-03-17 01:19:30 -0500

    Delete mousemode code

--- main/main.gd
+++ main/main.gd
@@ -11,7 +11,3 @@ func _ready() -> void:
 func _input(input: InputEvent) -> void:
 	if input.is_action_pressed("pause"):
 		pause.visible = !pause.visible
-		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		else:
-			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
