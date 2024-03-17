c2fb9f07651d21639988fe7a4cbc1190c2edf278
angelo <angelo.marney@gmail.com>
2024-03-16 20:01:53 -0500

    FoV scaling, jumping uses correct targspeed

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 class_name Player
 
 # TODO: Diving -> leftclick while sprinting.
+# TODO: Make `func`s non-local for performance if needed.
+# TODO: Compute initial `_movestate`, so that I can use `assert()` later.
 
 # Globally accessible instance; there is only one player.
 static var instance : Player
@@ -21,6 +23,7 @@ static var instance : Player
 @export_group("Sprint")
 @export_range(0.1, 100) var _sprint_speed : float
 @export_range(0.02, 5) var _time_to_sprint: float
+@export_range(75.1, 179.0) var _sprint_fov : float
 @export_group("Dive")
 @export_range(0.5, 10) var _dive_height : float
 @export_range(0, 20) var _dive_dist : float
@@ -38,6 +41,7 @@ var _mouse_relative : Vector2
 var _last_grab_attempt_time : float
 
 enum Movestate { WALK, SPRINT, JUMP_AIR, DIVE, DIVE_HITSTUN }
+var _ground_takeoff_horz_velocity : Vector2
 var _movestate : Movestate
 var _last_dive_start_time : float # Time units = seconds.
 var _last_dive_land_time : float
@@ -46,7 +50,6 @@ var _is_sprint_toggled : bool
 func _ready() -> void:
 	instance = self
 	assert(_interact_raycaster.target_position == _grab_range * Vector3.FORWARD)
-	# TODO: Compute initial `_movestate`, so that I can use `assert()` later.
 
 func _input(event : InputEvent) -> void:
 	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED: return
@@ -62,7 +65,7 @@ func _process(delta : float) -> void:
 	
 	match _movestate:
 		Movestate.WALK, Movestate.JUMP_AIR, Movestate.SPRINT:
-			# Standard FPS cam controls.
+			# Standard FPS cam controller.
 			const cam_sens = 0.2 # TODO: Make `cam_sens` changeable in settings menu.
 			cam.rotate_x(cam_sens * _mouse_relative.y * delta)
 			cam.rotation_degrees.x = clampf(cam.rotation_degrees.x, -_cam_pitch_max, _cam_pitch_max)
@@ -80,6 +83,13 @@ func _process(delta : float) -> void:
 			# Camera doesn't move while in hitstun.
 			pass
 	
+	# Cam FoV scales with with speed slightly.
+	var horz_speed := sqrt(velocity.z**2 + velocity.x**2)
+	var t := (horz_speed - _max_walk_speed) / (_sprint_speed - _max_walk_speed)
+	t = clamp(t, 0, 1)
+	const default_fov = 75.0
+	cam.fov = lerp(default_fov, _sprint_fov, t * t) # Quadratic ease-in
+	
 	# Since `_mouse_relative` is only updated when mouse position changes, we
 	# manually reset it right before the next frame using `call_deferred`.
 	(func() -> void: _mouse_relative = Vector2.ZERO).call_deferred()
@@ -87,7 +97,6 @@ func _process(delta : float) -> void:
 func _physics_process(delta : float) -> void:
 	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED: return
 	assert(_time_to_max_walk_speed >= delta) # no division by zero
-	#print(Vector2(velocity.x, velocity.z).length())
 	
 	print(
 		"WALK" if _movestate == Movestate.WALK else 
@@ -100,22 +109,20 @@ func _physics_process(delta : float) -> void:
 
 	const err_tol = 0.0001
 
-	var input_dir := Input.get_vector("move_leftwards", "move_rightwards", 
+	var move_input := Input.get_vector("move_leftwards", "move_rightwards", 
 		"move_forwards", "move_backwards")
 	var is_moving_forwards := (
-		input_dir != Vector2.ZERO 
-		and input_dir.angle_to(Vector2.UP) < PI/2 - err_tol
-		and input_dir.angle_to(Vector2.UP) > -PI/2 + err_tol
+		move_input != Vector2.ZERO 
+		and move_input.angle_to(Vector2.UP) < PI/2 - err_tol
+		and move_input.angle_to(Vector2.UP) > -PI/2 + err_tol
 	)
-	var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var move_input_dir := (transform.basis * Vector3(move_input.x, 0, move_input.y)).normalized()
 	
-	
-	# TODO: Make `func`s non-local for performance if needed.
 	var accelerate_velocity_in_move_dir := func(max_speed : float,
 		accel : float) -> void:
-		if move_dir: # nonzero
-			velocity.x = move_toward(velocity.x, move_dir.x * max_speed, accel * delta)
-			velocity.z = move_toward(velocity.z, move_dir.z * max_speed, accel * delta)
+		if move_input_dir: # nonzero
+			velocity.x = move_toward(velocity.x, move_input_dir.x * max_speed, accel * delta)
+			velocity.z = move_toward(velocity.z, move_input_dir.z * max_speed, accel * delta)
 		else:
 			velocity.x = move_toward(velocity.x, 0, accel * delta)
 			velocity.z = move_toward(velocity.z, 0, accel * delta)
@@ -136,14 +143,17 @@ func _physics_process(delta : float) -> void:
 			if _interact_raycaster.is_colliding() and !is_grab_successful:
 				print("interacted!")
 	
+	# Only call this after horizontal velocity is updated.
 	var handle_jump_input := func() -> void:
 		if Input.is_action_just_pressed("jump"):
 			assert(is_on_floor())
 			var jump_vel := sqrt(2 * _grav * _jump_height)
 			velocity.y = jump_vel
+			_ground_takeoff_horz_velocity = Vector2(velocity.x, velocity.z)
 	
 	match _movestate:
 		Movestate.WALK:
+			
 			handle_interact_and_grab_input.call()
 			
 			var accel := _max_walk_speed / _time_to_max_walk_speed
@@ -159,7 +169,9 @@ func _physics_process(delta : float) -> void:
 				Movestate.SPRINT if _is_sprint_toggled else
 				Movestate.WALK
 			)
+			
 		Movestate.SPRINT:
+			
 			var accel := _sprint_speed / _time_to_sprint
 			accelerate_velocity_in_move_dir.call(_sprint_speed, accel)
 			
@@ -177,13 +189,14 @@ func _physics_process(delta : float) -> void:
 				var t_up := dive_y_vel / _grav
 				var t_total := 2.0 * (t_up)
 				var dive_forward_vel := _dive_dist / t_total
-				velocity = dive_forward_vel * move_dir
+				velocity = dive_forward_vel * move_input_dir
+				_ground_takeoff_horz_velocity = Vector2(velocity.x, velocity.x)
 				velocity.y = dive_y_vel
 				
 				# Turn player body in dive direction.
 				var turn_anim := create_tween()
 				var target_rot := transform.looking_at(
-					global_position + move_dir
+					global_position + move_input_dir
 				).basis.get_euler()
 				turn_anim.tween_property(self, "rotation", 
 					target_rot, _dive_body_yaw_rot_time_length)
@@ -202,7 +215,9 @@ func _physics_process(delta : float) -> void:
 				Movestate.WALK if !_is_sprint_toggled else
 				Movestate.SPRINT
 			)
+			
 		Movestate.JUMP_AIR:
+			
 			handle_interact_and_grab_input.call()
 			
 			if !is_on_floor():
@@ -210,14 +225,24 @@ func _physics_process(delta : float) -> void:
 			
 			var accel := _midair_accel_multiplier * (_max_walk_speed 
 				/ _time_to_max_walk_speed)
-			accelerate_velocity_in_move_dir.call(_max_walk_speed, accel)
+			accelerate_velocity_in_move_dir.call(
+				_ground_takeoff_horz_velocity.length(), accel
+			)
+			
+			if Input.is_action_just_pressed("sprint"):
+				_is_sprint_toggled = true
+			
+			if is_on_floor() and _is_sprint_toggled and !is_moving_forwards: 
+				_is_sprint_toggled = false
 			
 			_movestate = (
 				Movestate.SPRINT if is_on_floor() and _is_sprint_toggled else
 				Movestate.WALK if is_on_floor() else 
 				Movestate.JUMP_AIR
 			)
+			
 		Movestate.DIVE:
+			
 			if is_on_floor():
 				_last_dive_land_time = Time.get_ticks_msec() / 1000.0
 				var dive_hitstun_anim := create_tween().set_parallel()
@@ -229,7 +254,9 @@ func _physics_process(delta : float) -> void:
 				Movestate.DIVE_HITSTUN if is_on_floor() else 
 				Movestate.DIVE
 			)
+			
 		Movestate.DIVE_HITSTUN:
+			
 			assert(is_on_floor()) # TODO: This gets hit when colliding into rabbit.
 								  # So make that impossible while diving.
 			var _is_hitstun_over := ((Time.get_ticks_msec() / 1000.0) 
@@ -238,4 +265,5 @@ func _physics_process(delta : float) -> void:
 				Movestate.WALK if _is_hitstun_over else
 				Movestate.DIVE_HITSTUN
 			)
+			
 	move_and_slide()
