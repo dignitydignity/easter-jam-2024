ee7966a542cf516ef67fa9c25b7214603f168939
dignity <legoguy88@gmail.com>
2024-03-16 23:37:34 -0700

    Added exit button and fixed pause button

--- pause/pause.gd
+++ pause/pause.gd
@@ -7,5 +7,16 @@ class_name Pause
 func resume_game():
 	visible = false
 	
+func exit_game():
+	get_tree().quit()
+	
+func toggle_mouse_visible():
+	if visible:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
 func _ready():
 	resume.pressed.connect(resume_game)
+	exit.pressed.connect(exit_game)
+	visibility_changed.connect(toggle_mouse_visible)
